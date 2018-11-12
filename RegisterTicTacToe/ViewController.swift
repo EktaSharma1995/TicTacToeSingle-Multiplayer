e2f13bb5c1ed040062b72b8b931aede72a49c368
Ekta Sharma <ektasharma@Ektas-MacBook-Air.local>
2018-11-11 20:22:32 -0500

    Tic Tac Toe single/Multiplayer

--- RegisterTicTacToe/ViewController.swift
+++ RegisterTicTacToe/ViewController.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ticTacToeView.isHidden = true
     }
 
-
+    
+    @IBOutlet weak var registerView: UIView!
+    
+    @IBOutlet weak var ticTacToeView: UIView!
+    
+    
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelAge: UILabel!
+    @IBOutlet weak var textFieldAge: UITextField!
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    @IBOutlet weak var displayName: UILabel!
+    
+    @IBOutlet weak var maleBtn: DLRadioButton!
+    
+    @IBOutlet weak var femaleBtn: DLRadioButton!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textFieldName.resignFirstResponder()
+        textFieldAge.resignFirstResponder()
+        return false
+    }
+    
+    @IBAction func segmentControlButton(_ sender: Any) {
+    if(segment.selectedSegmentIndex == 0){
+            ticTacToeView.isHidden = true
+            registerView.isHidden = false;
+        } else {
+            registerView.isHidden = true
+            ticTacToeView.isHidden = false
+        }
+    }
+    
+    
+    @IBAction func registerButton(_ sender: Any) {
+        displayName.text = "Welcome " + textFieldName.text!
+        registerView.isHidden = true
+        ticTacToeView.isHidden = false
+        segment.selectedSegmentIndex = 1
+    }
+    
+    
+    @IBAction func radioBtnAction(_ sender: DLRadioButton) {
+        if sender.tag == 1{
+            print("Male")
+        }else {
+            print("Female")
+        }
+    }
+    
+    
 }
 
